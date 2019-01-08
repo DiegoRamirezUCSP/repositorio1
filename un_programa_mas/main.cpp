7887a115688062acb1a3386d03835362b1450104
ALUMNO-M <ALUMNO-M@UCSP.AULAB>
2019-01-08 10:19:07 -0500

    first commit

---
+++ un_programa_mas/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){ system("color A");
+    int y;
+    int x;
+    cout << "valor de x : ";
+    cin >> x;
+    cout << "valor de y : ";
+    cin >> y;
+    /*cout << "valor de z : ";
+    cin >> z;*/
+    cout << endl;
+    cout << "el valor de x es " << x << endl;
+    cout << "el valor de y es " << y << endl;
+    //cout << "el valor de z es " << z << endl;
+    cout << endl;
+    cout << "x + y = " << (x + y) << endl;
+    cout << "x * y = " << (x * y) << endl;
+    cout << "x / y = " << (x/y) << endl;
+    cout << "x % y = " << (x%y) << endl;
+
+    return 0;
+}
